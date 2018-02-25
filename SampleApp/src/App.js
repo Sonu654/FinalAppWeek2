8e1ede4f7b46ae8e0969a126d5655f6f9dcc31e0
Suraj Sanwal <sonusanwal65@gmail.com>
2018-02-25 14:04:53 +0530

    SampleApp
    removed myTask
    
    Co-Authored-By: Sanwal2018 <sanwal2018@users.noreply.github.com>

---
+++ SampleApp/src/App.js
@@ -0,0 +1,10 @@
+import react {Component} from "react";
+import RouterComponent from '/RouterComponent";
+
+
+export default class App extends Component{
+
+    render(){
+        <RouterComponent />
+    }
+}
